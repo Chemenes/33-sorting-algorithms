27b297e4875fc7c4f1508e088041dbfaa11a3b0b
Christopher <conrad317@gmail.com>
2018-08-11 17:24:25 -0700

    complete

--- src/__test__/bubblesort.test.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-
-import { bubblesort } from '../lib/bubblesort';
--- src/lib/bubblesort.js
+++ src/lib/bubblesort.js
@@ -16,3 +16,4 @@ function bubbleSort(arr) {
   return arr;
 }
 bubbleSort([6, 7, 3, 5, 4, 8,]); //[3, 4, 5, 6, 7, 8]
+// .
